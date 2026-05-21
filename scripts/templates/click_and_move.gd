5e27f68765f4e5c4e8f30c0cb22ec5c5d0575ca9
raptorettes <gutterpercher@gmail.com>
2026-05-21 16:49:36 +0200

    extra scripts and scenes

--- scripts/templates/click_and_move.gd
+++ scripts/templates/click_and_move.gd
@@ -1,1 +1,29 @@
-extends Node
+extends CharacterBody2D
+
+#This uses only the mouse to control the character. 
+#Clicking on the screen will cause the player to move to the target location.
+#
+#Note the distance_to() check we make prior to movement. Without this test, 
+#the body would "jitter" upon reaching the target position, as it moves slightly past the position and tries to move back, 
+#only to move too far and repeat.
+#
+#Uncommenting the look_at() line will also turn the body to point in its direction of motion if you prefer.
+#Tip
+#This technique can also be used as the basis of a "following" character. 
+#The target position can be that of any object you want to move to.
+
+
+@export var speed = 400
+
+var target = position
+
+func _input(event):
+	# Use is_action_pressed to only accept single taps as input instead of mouse drags.
+	if event.is_action_pressed(&"click"):
+		target = get_global_mouse_position()
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	if position.distance_to(target) > 10:
+		move_and_slide()
--- scripts/templates/rotation_movement_mouse.gd
+++ scripts/templates/rotation_movement_mouse.gd
@@ -1,1 +1,21 @@
-extends Node
+extends CharacterBody2D
+
+#Rotation + movement (mouse)
+#
+#This style of movement is a variation of the previous one. 
+#This time, the direction is set by the mouse position instead of the keyboard. 
+#The character will always "look at" the mouse pointer. The forward/back inputs remain the same, however.
+
+#Here we're using the Node2D look_at() method to point the player towards the mouse's position. 
+#Without this function, you could get the same effect by setting the angle like this:
+#rotation = get_global_mouse_position().angle_to_point(position)
+
+@export var speed = 400
+
+func get_input():
+	look_at(get_global_mouse_position())
+	velocity = transform.x * Input.get_axis("down", "up") * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
